f56f02e00a741952fb533354f5e3bedca95e3c5c
Marco Schincaglia <orione-team+sch@xpeppers.com>
2013-01-24 22:49:14 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '6e9de0c2c63503132be61e768623852549f1403aa565981980078e0605aa37d661b8e85a6763eba0799d1a44977f1f74258120780aa1be0a380bf5661ce18c6a'
---
+++ test/functional/locator_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LocatorControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
